0866eca076c7a00aab5d522c8c75c2944f190244
dangherve <dangherve@gmail.com>
2026-03-07 01:21:08 -0500

    autorebase tools

---
+++ buildTools/rebase.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+git remote add upstream https://github.com/Fearinn/gemsofiridescia.git
+git fetch upstream
+git rebase upstream/main
+git remote set-url origin git@github.com:dangherve/gemsofiridescia.git
+git push --force --set-upstream origin main
